4b6b351086a232ed1752eb34fbbe4af642e0cfad
analben <analmanakiwala@gmail.com>
2016-06-01 09:18:01 -0400

    first push

---
+++ public/app.js
@@ -0,0 +1,49 @@
+$.getJSON('/articles', function(data) {
+  for (var i = 0; i<data.length; i++){
+    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
+  }
+});
+
+
+$(document).on('click', 'p', function(){
+  $('#notes').empty();
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#notes').prepend('<h2>' + data.title + '</h2>');
+      $('#notes').prepend('<input id="titleinput" name="title" >');
+      $('#notes').prepend('<textarea id="bodyinput" name="body"></textarea>');
+      $('#notes').prepend('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+
+      if(data.note){
+        $('#titleinput').val(data.note.title);
+        $('#bodyinput').val(data.note.body);
+      }
+    });
+});
+
+$(document).on('click', '#savenote', function(){
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $('#titleinput').val(),
+      body: $('#bodyinput').val()
+    }
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#notes').empty();
+    });
+
+
+  $('#titleinput').val("");
+  $('#bodyinput').val("");
+});
